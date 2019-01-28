53c5aaab39204d631a686d4890ec7e232e254b81
Chris LaRose <cjlarose@gmail.com>
2019-01-27 19:32:30 -0800

    Get the tests green

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,34 +1,24 @@
 class Bottles
   def verse(verse_number)
-    if verse_number == 99
+    if verse_number == 0
       <<~VERSE
-        99 bottles of beer on the wall, 99 bottles of beer.
-        Take one down and pass it around, 98 bottles of beer on the wall.
-      VERSE
-    elsif verse_number == 98
-      <<~VERSE
-        98 bottles of beer on the wall, 98 bottles of beer.
-        Take one down and pass it around, 97 bottles of beer on the wall.
-      VERSE
-    elsif verse_number == 89
-      <<~VERSE
-        89 bottles of beer on the wall, 89 bottles of beer.
-        Take one down and pass it around, 88 bottles of beer on the wall.
-      VERSE
-    elsif verse_number == 2
-      <<~VERSE
-        2 bottles of beer on the wall, 2 bottles of beer.
-        Take one down and pass it around, 1 bottle of beer on the wall.
+        No more bottles of beer on the wall, no more bottles of beer.
+        Go to the store and buy some more, 99 bottles of beer on the wall.
       VERSE
     elsif verse_number == 1
       <<~VERSE
         1 bottle of beer on the wall, 1 bottle of beer.
         Take it down and pass it around, no more bottles of beer on the wall.
       VERSE
-    elsif verse_number == 0
+    elsif verse_number == 2
       <<~VERSE
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
+        2 bottles of beer on the wall, 2 bottles of beer.
+        Take one down and pass it around, 1 bottle of beer on the wall.
+      VERSE
+    else
+      <<~VERSE
+        #{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.
+        Take one down and pass it around, #{verse_number - 1} bottles of beer on the wall.
       VERSE
     end
   end
